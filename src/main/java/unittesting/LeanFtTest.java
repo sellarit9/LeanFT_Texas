fd777be31af8fb9f32ed3b1330e7a7979e8de9fd
tre <sa@nga>
2018-05-18 20:19:12 +0000

    Moved LeanFT class and TexasAppModel to under main

---
+++ src/main/java/unittesting/LeanFtTest.java
@@ -0,0 +1,62 @@
+package unittesting;
+
+import com.hp.lft.sdk.web.Browser;
+import com.hp.lft.sdk.web.BrowserType;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.hp.lft.sdk.*;
+
+public class LeanFtTest extends UnitTestClassBase
+{
+
+    private static String url = "https://texas.gov/";
+    private static Browser browser;
+
+    public LeanFtTest()
+    {
+        //Change this constructor to private if you supply your own public constructor
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        instance = new LeanFtTest();
+        globalSetup(LeanFtTest.class);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        globalTearDown();
+    }
+
+//    @Before
+//    public void setUp() throws Exception
+//    {
+//        browser = BrowserFactory.launch(BrowserType.CHROME);
+//    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        browser = SrfLab.launchBrowser(BrowserType.CHROME);
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        browser.close();
+    }
+
+    @Test
+    public void test() throws GeneralLeanFtException
+    {
+        browser.navigate(url);
+
+        TexasAppModel appModel = new TexasAppModel(browser);
+        appModel.texasGovTheOfficialWebsiteOfTheStateOfTexasPage().infoLink().click();
+        appModel.texasGovTheOfficialWebsiteOfTheStateOfTexasPage().texasImage().click();
+    }
+
+}
